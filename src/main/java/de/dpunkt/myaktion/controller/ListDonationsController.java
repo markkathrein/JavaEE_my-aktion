1b7e8998b6478ddd1d6fcab723bf65f1c3eaaa1a
link <link@192.168.1.104>
2017-07-17 14:56:41 +0200

    erweiterung, Klassen usw..

---
+++ src/main/java/de/dpunkt/myaktion/controller/ListDonationsController.java
@@ -0,0 +1,16 @@
+package de.dpunkt.myaktion.controller;
+
+import de.dpunkt.myaktion.model.Donation.Status;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@SessionScoped
+@Named
+public class ListDonationsController implements Serializable {
+	private static final long serialVersionUID = 437878972432L;
+
+	public String doOk() {
+		return Pages.LIST_CAMPAIGNS;
+	}
+}
